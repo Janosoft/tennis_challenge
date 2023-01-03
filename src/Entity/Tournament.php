7748efdb4a156c357c335c52bff7dcb455b2a747
Alejandro Lodes <janosoft@gmail.com>
2023-01-03 20:10:12 -0300

    limpieza de código

--- src/Entity/Tournament.php
+++ src/Entity/Tournament.php
@@ -2,15 +2,12 @@
 
 namespace App\Entity;
 
-use Faker;
 use App\Repository\TournamentRepository;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
-use function PHPUnit\Framework\isNull;
 
 #[ORM\Entity(repositoryClass: TournamentRepository::class)]
 class Tournament
